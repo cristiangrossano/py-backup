13f7f70d9ef100f4dd9cad5e720e3326cf36912c
Cristian Grossano <81424772+cristiangrossano@users.noreply.github.com>
2023-01-12 01:06:59 +0100

    Added subfolder check

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import os
 import shutil
 
 # Add path to folders
-local_folder = "./testA"
-destination_folder = "./testB"
+local_folder = "./TestA"
+destination_folder = "./TestB"
 count = 0
 
 print("Copying files from " + local_folder + " to " + destination_folder)
@@ -13,9 +13,12 @@ local_files = os.listdir(local_folder)
 for file_name in local_files:
     local_file = os.path.join(local_folder, file_name)
     destination_file = os.path.join(destination_folder, file_name)
-    if not os.path.exists(destination_file):
-        shutil.copy2(local_file, destination_folder)
-        count = count + 1
+    if os.path.isdir(local_file):
+        shutil.copytree(local_file, destination_file)
+    else:
+        if not os.path.exists(destination_file):
+            shutil.copy2(local_file, destination_folder)
+            count = count + 1
 
 print("Completed!")
-print(str(count) + "was successfully copied.")
+print(str(count) + " files was successfully copied.")
